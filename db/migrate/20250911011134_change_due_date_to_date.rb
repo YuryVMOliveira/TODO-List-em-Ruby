32051ebb75de17a10dcbf69963280034e1ae36cd
Yury Vieira Marques Oliveira <yuryolyvy@gmail.com>
2025-09-10 22:12:52 -0300

    retirado a hora de due_date para ficar so a data

---
+++ db/migrate/20250911011134_change_due_date_to_date.rb
@@ -0,0 +1,5 @@
+class ChangeDueDateToDate < ActiveRecord::Migration[8.0]
+  def change
+    change_column :tasks, :due_date, :date
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_10_025219) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_11_011134) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -25,7 +25,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_10_025219) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.datetime "due_date"
+    t.date "due_date"
     t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
